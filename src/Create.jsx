296605dd3676a0a74f10fbd78f53655d92a7baa9
gulomovx <xgulomov447@gmail.com>
2023-09-28 14:05:15 +0500

    crud-list

--- src/Create.jsx
+++ src/Create.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "./UserReducer";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Create = () => {
   const [name, setName] = useState("");
@@ -18,7 +18,7 @@ const Create = () => {
     e.preventDefault();
     dispatch(addUser({ id: users[users.length - 1].id + 1, name, email }));
     navigate("/");
-    console.log("sdfcv");
+    console.log(users);
   };
 
   return (
@@ -50,14 +50,19 @@ const Create = () => {
           {/* <button className="bg-blue-500 px-6 py-2 rounded">
             Submit
           </button> */}
-          <a href="#_" class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
-<span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-<span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-<span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-<span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Submit</span>
-</a>
+          <a class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
+            <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+            <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+            <span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+            <button
+              type="submit"
+              class="relative transition-colors duration-300 delay-200 group-hover:text-white ease"
+            >
+              Submit
+            </button>
+          </a>
         </form>
       </div>
     </div>
